d6816a7c602af89b161d3969ae863181646e51a3
Dmitriy <trishkindima098@gmail.com>
2021-12-10 00:04:24 +0300

    Добавление класса ProductInputScreen

--- Final/Final/ProductInputScreen.h
+++ Final/Final/ProductInputScreen.h
@@ -1,1 +1,19 @@
 #include "ProductList.h"
+
+class ProductInputScreen
+{
+private:
+	ProductList* ptrProductList;
+	string tName;
+	string tType;
+	string tMaterial;
+	double tPrice, tSize;
+	int tNumber;
+public:
+	ProductInputScreen(ProductList* ptrTL) : ptrProductList(ptrTL)
+	{
+		/* тут пусто */
+	}
+	void setProduct(); // добавить данные о жильце
+};
+
